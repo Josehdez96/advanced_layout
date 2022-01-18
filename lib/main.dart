0f585609306f360eaef2d4fb5f19505704e0f9b2
Jose Daniel Hernandez <josedanielhdezit@gmail.com>
2022-01-17 19:48:35 -0500

    tables

--- lib/main.dart
+++ lib/main.dart
@@ -4,128 +4,61 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  static const int numItems = 10;
+  List<bool> selected = List<bool>.generate(numItems, (index) => false);
+
+  Color? _getRowColor(Set<MaterialState> states, int index) {
+    if (states.contains(MaterialState.selected)) {
+      return Colors.red;
+    }
+    if (index.isEven) {
+      return Colors.grey.withOpacity(0.3);
+    }
+    return null;
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Center(
-        child: FractionallySizedBox(
-          widthFactor: 0.7,
-          child: GridView.count(
-            shrinkWrap: true,
-            crossAxisSpacing: 3,
-            mainAxisSpacing: 3,
-            crossAxisCount: 3,
-            children: [
-              Container(
-                color: Colors.white,
-                child: Center(
-                  child: Text(
-                    'X',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 40,
-                      inherit: false,
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                color: Colors.white,
-              ),
-              Container(
-                color: Colors.white,
-              ),
-              Container(
-                color: Colors.white,
-              ),
-              Container(
-                color: Colors.white,
-              ),
-              Container(
-                color: Colors.white,
-              ),
-              Container(
-                color: Colors.white,
-              ),
-              Container(
-                color: Colors.white,
-              ),
-              Container(
-                color: Colors.white,
+      home: Scaffold(
+        body: Center(
+          child: DataTable(
+            columns: const [
+              DataColumn(
+                label: Text('Number'),
               ),
             ],
+            rows: List<DataRow>.generate(
+              numItems,
+              (index) => DataRow(
+                color: MaterialStateProperty.resolveWith<Color?>(
+                  (states) => _getRowColor(states, index),
+                ),
+                cells: [
+                  DataCell(
+                    Text('Row $index'),
+                  ),
+                ],
+                selected: selected[index],
+                onSelectChanged: (value) {
+                  setState(() {
+                    selected[index] = value!;
+                  });
+                },
+              ),
+            ),
           ),
         ),
       ),
     );
-    // return MaterialApp(
-    //   title: 'Flutter Demo',
-    //   debugShowCheckedModeBanner: false,
-    //   theme: ThemeData.light(),
-    //   home: Container(
-    //     alignment: Alignment.center,
-    //     color: Colors.blue,
-    //     child: Container(
-    //       color: Colors.red,
-    //       child: Align(
-    //         alignment: const Alignment(0.7, 0),
-    //         heightFactor: 2,
-    //         child: Container(
-    //           color: Colors.yellow,
-    //           height: 100,
-    //           width: 100,
-    //           child: Center(
-    //             child: GridView.count(
-    //               crossAxisSpacing: 1,
-    //               // mainAxisSpacing: 2,
-    //               crossAxisCount: 3,
-    //               children: [
-    //                 Container(
-    //                   color: Colors.amber,
-    //                 ),
-    //                 Container(
-    //                   color: Colors.red,
-    //                 ),
-    //                 Container(
-    //                   color: Colors.green,
-    //                 ),
-    //                 Container(
-    //                   color: Colors.lightBlue,
-    //                 ),
-    //                 Container(
-    //                   color: Colors.blue,
-    //                 ),
-    //                 Container(
-    //                   color: Colors.pink,
-    //                 ),
-    //                 Container(
-    //                   color: Colors.purple,
-    //                 ),
-    //                 Container(
-    //                   color: Colors.deepOrange,
-    //                 ),
-    //                 Container(
-    //                   color: Colors.deepPurple,
-    //                 ),
-    //               ],
-    //             ),
-    //             // child: Text(
-    //             //   'hola',
-    //             //   style: TextStyle(
-    //             //     inherit: false,
-    //             //     fontSize: 32,
-    //             //     color: Colors.black,
-    //             //   ),
-    //             // ),
-    //           ),
-    //         ),
-    //       ),
-    //     ),
-    //   ),
-    // );
   }
 }
